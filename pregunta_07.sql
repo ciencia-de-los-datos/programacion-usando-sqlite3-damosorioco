c96e1e4caac296284d6e598e2408142988567c38
Danilo Osorio <damosorioco@unal.edu.co>
2022-05-23 23:22:08 -0500

    p7p8
    p7 p8

--- pregunta_07.sql
+++ pregunta_07.sql
@@ -43,4 +43,4 @@
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT * FROM tbl1 WHERE K0 <> "A","B" AND WHERE c13 <> 900 AND <> 200 ORDER BY c14;
+SELECT * FROM tbl1 WHERE K0 <> "A" AND <> "B" AND WHERE c13 <> 900 AND <> 200 ORDER BY c14;
